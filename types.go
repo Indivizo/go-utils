528651981b8e1dd7783e1bcc8ef04f6eead06e2f
Kalman Hosszu <hosszu.kalman@gmail.com>
2016-02-16 11:24:01 +0100

    Create Url type and Validate methods

--- types.go
+++ types.go
@@ -1,1 +1,18 @@
 package go_utils
+
+import (
+	"github.com/asaskevich/govalidator"
+)
+
+type Url string
+
+func (url Url) String() string {
+	return (string)(url)
+}
+
+func (url Url) Validate() error {
+	if !govalidator.IsRequestURL(url.String()) {
+		return ErrInvalidUrl
+	}
+	return nil
+}
